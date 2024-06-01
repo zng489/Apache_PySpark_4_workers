8341e0fc1ab073121c2ce145198ea47b397709be
zng489 <45658785+zng489@users.noreply.github.com>
2024-05-31 22:08:46 -0300

    Add files via upload

---
+++ Apache_PySpark_4_workers/Apache_PySpark/build.sh
@@ -0,0 +1,35 @@
+# -- Software Stack Version
+
+SPARK_VERSION="3.4.0"
+HADOOP_VERSION="3"
+JUPYTERLAB_VERSION="3.6.3"
+
+# -- Building the Images
+
+docker build \
+  -f cluster-base.Dockerfile \
+  -t cluster-base .
+
+docker build \
+  --build-arg spark_version="${SPARK_VERSION}" \
+  --build-arg hadoop_version="${HADOOP_VERSION}" \
+  -f spark-base.Dockerfile \
+  -t spark-base .
+
+docker build \
+  -f spark-master.Dockerfile \
+  -t spark-master .
+
+docker build \
+  -f spark-worker.Dockerfile \
+  -t spark-worker .
+
+docker build \
+  --build-arg spark_version="${SPARK_VERSION}" \
+  --build-arg jupyterlab_version="${JUPYTERLAB_VERSION}" \
+  -f jupyterlab.Dockerfile \
+  -t jupyterlab .
+
+# Local copy of Notebooks and job-submit scripts outside Git change tracking
+##mkdir -p ./local/notebooks
+##cp -R ./notebooks/* ./local/notebooks
---
+++ Apache_PySpark_4_workers/Apache_PySpark/spark-master.Dockerfile
@@ -0,0 +1,11 @@
+FROM spark-base
+
+# -- Runtime
+
+ARG spark_master_web_ui=8080
+
+EXPOSE ${spark_master_web_ui} ${SPARK_MASTER_PORT}
+CMD bin/spark-class org.apache.spark.deploy.master.Master >> logs/spark-master.out
+
+#Create the workspace/events shared dir and start Spark Master
+###CMD bash -c "mkdir -p /opt/workspace/events && mkdir -p /opt/workspace/datain && mkdir -p /opt/workspace/dataout && bin/spark-class org.apache.spark.deploy.master.Master >> logs/spark-master.out"
